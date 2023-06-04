44de0b04745ab95c2d9cd97cb1eb57a2a37a85c2
FabioSR29 <fabiosalazarruiz@gmail.com>
2023-06-03 20:44:30 -0600

    je fue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           The Corner of Idillas #Nojodaaaaj pura tierra aqui tambien nojodaaaa
         </p>
